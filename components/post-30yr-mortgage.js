6ad4cd1fa6997e56f6d26a1c33185eece3f36344
gillcaleb <gillcmc21@gmail.com>
2023-03-17 18:47:45 +0000

    adding mortgage

---
+++ components/post-30yr-mortgage.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+function MortgageRateCard() {
+  const [mortgageRate, setMortgageRate] = useState(null);
+  const [date, setDate] = useState(null);
+
+  useEffect(() => {
+    async function fetchMortgageRate() {
+      const response = await fetch("http://localhost:8000/mortgage_rate");
+      const data = await response.json();
+      setMortgageRate(data.mortgage_rate);
+      setDate(data.date);
+    }
+    fetchMortgageRate();
+  }, []);
+
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Current Mortgage Rate</h5>
+        <p className="card-text">
+          {date && `As of ${date}: `}
+          {mortgageRate ? `${mortgageRate}%` : "Loading..."}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default MortgageRateCard;
+
