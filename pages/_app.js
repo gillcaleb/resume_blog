bb09839531e6b11b16fe858e3a5e6b16b25d0100
gillcaleb <gillcmc21@gmail.com>
2025-01-21 09:42:23 -0500

    Etchgpt (#5)
    * etch gpt and ga
    
    * adding underline

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/index.css'
+import GoogleAnalytics from '../components/google-analytics';
 import { ThemeProvider } from "next-themes";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
+      <GoogleAnalytics />
       <Component {...pageProps} />
     </ThemeProvider>
   );
